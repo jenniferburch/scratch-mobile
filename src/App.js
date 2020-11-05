75e9910fc3e780d93bf9da4e4f372bc2b93feaf4
Jennifer Burch <jennifer.burch@whitecupsolutions.com>
2020-11-05 14:17:57 -0800

    minor formatting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import { SafeAreaView, View, Text, StatusBar } from 'react-native';
+import { SafeAreaView, View, Text, StatusBar, StyleSheet } from 'react-native';
 
 
 const App: () => React$Node = () => {
@@ -15,13 +15,26 @@ const App: () => React$Node = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
+        <View style={styles.header}>
+          <Text style={styles.headerText}> Hello React </Text>
+        </View>
         <View>
-          <Text> Hello React </Text>
+          <Text>text 2</Text> 
         </View>
       </SafeAreaView>
     </>
   );
 };
 
-
+const styles = StyleSheet.create({
+  header: {
+    borderWidth: .5,
+    borderColor: 'black',
+    padding: 10,
+    margin: 5
+  },
+  headerText: {
+    textAlign: 'center',
+  }
+})
 export default App;
